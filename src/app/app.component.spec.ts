3fd48cd01b49a8d9cd4403d0e4216dfc117ee3e8
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2022-02-18 13:55:38 +0100

    operators

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'rxjs-workshop-1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('rxjs-workshop-1');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rxjs-workshop-1 app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { from, of, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxjs-workshop-1';
+
+  of(): void {
+    of('of')
+      .subscribe(console.log);
+  }
+
+  from(): void {
+    const p = new Promise(resolve =>
+      setTimeout(() => resolve('from'), 500)
+    );
+    from(p)
+      .subscribe(console.log);
+  }
+
+  combineLatest(): void {
+  }
+
+  concat(): void {
+  }
+
+  merge(): void {
+  }
+
+  map(): void {
+  }
+
+  switchMap(): void {
+  }
+
+  bufferCount(): void {
+  }
+
+  bufferTime(): void {
+  }
+
+  scan(): void {
+  }
+
+  filter(): void {
+  }
+
+  distinctUntilChanged(): void {
+  }
+
+  takeUntil(): void {
+  }
+
+  first(): void {
+  }
+
+  skip(): void {
+  }
+
+  auditTime(): void {
+  }
+
+  debounceTime(): void {
+  }
+
+  throttleTime(): void {
+  }
+
+  startWith(): void {
+  }
+
+  share(): void {
+  }
+
+  catchError(): void {
+  }
+
+  tap(): void {
+  }
+
+  clear() {
+    console.clear();
+  }
 }
